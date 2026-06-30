bda62984a0427c123bfc17e1a9b82ae0abe4c483
nik-inholland <745045@student.inholland.nl>
2026-06-30 13:39:21 +0200

    update

--- WebApplication3/Models/ViewModels/TableViewModel.cs
+++ WebApplication3/Models/ViewModels/TableViewModel.cs
@@ -1,23 +1,8 @@
 ﻿namespace WebApplication3.Models.ViewModels
 {
-    public class MenuViewModel
+    public class TableViewModel
     {
-        public List<MenuItem> MenuItems { get; set; } = new List<MenuItem>();
-
-       
-        public string SelectedCard { get; set; } = "All";          
-        public int SelectedCourseType { get; set; } = 0;           
-
-        
-        public bool HasItems => MenuItems.Any();
-
-        public MenuViewModel() { }
-
-        public MenuViewModel(List<MenuItem> menuItems, string selectedCard = "All", int selectedCourse = 0)
-        {
-            MenuItems = menuItems;
-            SelectedCard = selectedCard;
-            SelectedCourseType = selectedCourse;
-        }
+        public int TableID { get; set; }
+        public TableStatus Occupied { get; set; }
     }
 }
